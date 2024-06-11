c87bd9bb3ae4901d844bd1ca883d7c63534f0fe7
Abhishek <Abhishekraut8697@gmail.com>
2024-06-11 17:08:14 +0530

    created demo filter functionality

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,8 +1,17 @@
-import { CDN_URL } from "../utils/constant"
+import React, { useState } from 'react';
+import { CDN_URL } from "../utils/constant";
+import RestorCard from "./RestroCard";
+import resData from '../utils/resinfo.json';
 
-import RestorCard from "./RestroCard"
-import resData from '../utils/resinfo.json'
 const Body = () => {
+    const [filteredData, setFilteredData] = useState(resData);
+
+    const handleSearch = () => {
+        const filteredResults = resData.filter((res) => res.info.avgRating > 4.5);
+        setFilteredData(filteredResults);
+        console.log(filteredResults);
+    };
+
     return (
         <div className='body'>
             <div className='search'>
@@ -10,9 +19,10 @@ const Body = () => {
                     <i className="ri-search-line"></i>
                     <input type='text' placeholder='Search for restaurants, cuisines and more' />
                 </div>
+                <button className="search-btn" onClick={handleSearch}>Search</button>
             </div>
             <div className='res-container'>
-                {resData.map((restaurant) => (
+                {filteredData.map((restaurant) => (
                     <RestorCard
                         key={restaurant.info.id}
                         name={restaurant.info.name}
@@ -25,7 +35,7 @@ const Body = () => {
                 ))}
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Body;
